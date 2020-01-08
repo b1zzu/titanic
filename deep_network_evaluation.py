949f83aefe6d5350d083937ae74c11184e7a1b15
Davide <david3bizzarri@gmail.com>
2020-01-08 15:43:42 +0100

    rename to evaluation

--- deep_network_evaluation.py
+++ deep_network_evaluation.py
@@ -4,9 +4,11 @@ import constants
 import util
 import numpy
 
+[x_prove, y_prove] = util.read(constants.PROVE)
+[x_train, y_train] = util.read(constants.TRAIN)
+[x_test, y_test] = util.read(constants.TEST)
 
 # %%
-[x_train, y_train] = util.read(constants.TRAIN)
 model = keras.models.load_model('deep_model.h5')
 
 # model.summary()
@@ -15,9 +17,7 @@ model = keras.models.load_model('deep_model.h5')
 model.evaluate(x_train.values, numpy.asarray(y_train))
 
 # %%
-[x_test, y_test] = util.read(constants.TEST)
 model.evaluate(x_test.values, numpy.asarray(y_test))
 
 # %%
-[x_prove, y_prove] = util.read(constants.PROVE)
 model.evaluate(x_prove.values, numpy.asarray(y_prove))
